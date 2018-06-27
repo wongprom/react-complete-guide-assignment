495aedeba9a7d21b8cd1bf34dd2c0a76938e0543
wongprom <jimmybackstrom@hotmail.com>
2018-06-27 14:03:57 +0200

    complete steps in assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Input from './UserInput/UserInput';
+import Output from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    peoples: [
+      {name: 'Jimmy'},
+      {name: 'Peter'},
+    ]
+  }
+
   render() {
+    const oneGirl = 'Vanessa';
     return (
       <div className="App">
-       
+        <Input />
+        <Output 
+          peopleZero = {this.state.peoples[0].name}
+          oneGirl = {oneGirl}
+          peopleOne = {this.state.peoples[1].name} />
+        <Output 
+          //oneGirl = {oneGirl} 
+          />
+        <Output 
+          //peopleOne = {this.state.peoples[1].name}
+          />
       </div>
     );
   }
