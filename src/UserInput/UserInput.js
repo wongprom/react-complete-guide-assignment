b4a62e7de4c09abac0630ab1033bbd793bcc2a7d
wongprom <jimmybackstrom@hotmail.com>
2018-06-27 14:01:25 +0200

    Add 2 components
    Components name UserInput & UserOutput. Output data with props and state

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Input = () => {
+    return(
+        <div>
+            <input type='text'/>
+        </div>
+    );
+}
+export default Input;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Output = (props) => {
+    return(
+        <div>
+            <p>{props.oneGirl}</p>
+            <p>{props.peopleZero}</p>
+            <p>{props.peopleOne}</p>
+        </div>
+    );
+}
+
+export default Output;
