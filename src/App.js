a2db1cc0aacb42a4a7aaf8fe4892fa1047d24879
wongprom <jimmybackstrom@hotmail.com>
2018-07-02 21:08:01 +0200

    Setup workspace for assignment 2

--- src/App.js
+++ src/App.js
@@ -4,51 +4,19 @@ import Input from './UserInput/UserInput';
 import Output from './UserOutput/UserOutput';
 
 class App extends Component {
-  state = {
-    peoples: [
-      {name: 'Jimmy Wongprom'},
-      {name: 'Pelle'},
-    ]
-  }
-
-  switchNameWithInput  = (event) => {
-    this.setState({
-      peoples: [
-        {name: event.target.value},
-        {name: 'Pelle'},
-      ]
-    }) 
-}
-
-  changeNameHandler = () => {
-    this.setState({
-      peoples: [
-        {name: 'Sune Bäckström'},
-        {name: 'Pelle Svanslös'},
-      ]
-    })
-  }
-
-  
 
   render() {
-    const oneGirl = 'Vanessa';
-
-    
-
+  
     return (
       <div className="App">
-        <Input 
-          test = {this.switchNameWithInput}
-          currentName = {this.state.peoples[0].name} />
-        <Output 
-          changeName = {this.changeNameHandler}
-          peopleZero = {this.state.peoples[0].name}
-          oneGirl = {oneGirl}
-          peopleOne = {this.state.peoples[1].name} 
-           />
-        <Output animal = 'Cat' />
-        <Output animal= "Dog" />
+        <ol>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+        </ol>
       </div>
     );
   }
